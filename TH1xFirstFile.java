ae46ec7b6b051b7304b430f0a0795d01b4338198
pham dat <mrphamdat099@gmail.com>
2019-08-29 09:09:00 +0700

    Hiển thị thời gian của hệ thống

---
+++ TH1xFirstFile.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class TH1xFirstFile {
+    public static void main(String[] args) {
+        Date time = new Date();
+        System.out.println("Now time is: "+ time);
+    }
+}
