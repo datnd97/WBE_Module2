5b3b83992c40196aa71ea98fba7e0410581e7ee5
pham dat <mrphamdat099@gmail.com>
2019-08-29 10:40:51 +0700

    [Thực hành] Tính số ngày trong tháng

--- TH5_SwitchCase.java
+++ TH5_SwitchCase.java
@@ -6,10 +6,11 @@ public class TH5_SwitchCase {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Which month that you want to count day: ");
         int month = scanner.nextInt();
+        String daysInMonth;
         switch (month)
         {
             case 2:
-                System.out.print("The month '2' has 28 or 29 days!" );
+                daysInMonth = "28 or 29";
             break;
             case 1:
             case 3:
@@ -18,17 +19,25 @@ public class TH5_SwitchCase {
             case 8:
             case 10:
             case 12:
-                System.out.print("The month '"+month+"' has 31 days!");
+                daysInMonth = "31";
                 break;
             case 4:
             case 6:
             case 9:
             case 11:
-                System.out.print("The month '"+month+"' has 30 days!");
+                daysInMonth = "30";
                 break;
             default:
-                System.out.print("Invalid input");
+                daysInMonth="";
                 break;
+        }
+        if(daysInMonth != "")
+        {
+            System.out.printf("The month '%d' has %s days!",month,daysInMonth);
+        }
+        else
+        {
+            System.out.println("Invaild input");
         }
     }
 }
